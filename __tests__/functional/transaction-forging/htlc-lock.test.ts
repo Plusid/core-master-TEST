1bc723ceb741c7ea557f7c61c2eb31492709c504
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-05-17 09:35:24 -0600

    1e8 to 1e5

--- __tests__/functional/transaction-forging/htlc-lock.test.ts
+++ __tests__/functional/transaction-forging/htlc-lock.test.ts
@@ -13,7 +13,7 @@ afterAll(support.tearDown);
 describe("Transaction Forging - HTLC Lock", () => {
     it("should broadcast, accept and forge it [Signed with 1 Passphase]", async () => {
         // Initial Funds
-        const initialFunds = TransactionFactory.transfer(Identities.Address.fromPassphrase(passphrase), 100 * 1e8)
+        const initialFunds = TransactionFactory.transfer(Identities.Address.fromPassphrase(passphrase), 100 * 1e5)
             .withPassphrase(secrets[0])
             .createOne();
 
@@ -42,7 +42,7 @@ describe("Transaction Forging - HTLC Lock", () => {
         const passphrase = secondPassphrase;
 
         // Initial Funds
-        const initialFunds = TransactionFactory.transfer(Identities.Address.fromPassphrase(passphrase), 100 * 1e8)
+        const initialFunds = TransactionFactory.transfer(Identities.Address.fromPassphrase(passphrase), 100 * 1e5)
             .withPassphrase(secrets[0])
             .createOne();
 
@@ -77,7 +77,7 @@ describe("Transaction Forging - HTLC Lock", () => {
 
     it("should broadcast, accept and forge it [3-of-3 multisig]", async () => {
         // Funds to register a multi signature wallet
-        const initialFunds = TransactionFactory.transfer(Identities.Address.fromPassphrase(secrets[3]), 50 * 1e8)
+        const initialFunds = TransactionFactory.transfer(Identities.Address.fromPassphrase(secrets[3]), 50 * 1e5)
             .withPassphrase(secrets[0])
             .createOne();
 
@@ -106,7 +106,7 @@ describe("Transaction Forging - HTLC Lock", () => {
         const multiSigAddress = Identities.Address.fromMultiSignatureAsset(multiSignature.asset.multiSignature);
         const multiSigPublicKey = Identities.PublicKey.fromMultiSignatureAsset(multiSignature.asset.multiSignature);
 
-        const multiSignatureFunds = TransactionFactory.transfer(multiSigAddress, 20 * 1e8)
+        const multiSignatureFunds = TransactionFactory.transfer(multiSigAddress, 20 * 1e5)
             .withPassphrase(secrets[0])
             .createOne();
 
