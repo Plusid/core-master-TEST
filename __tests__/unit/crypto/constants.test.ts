ec9c73bc1faf262222dce2dce378f06310f405c0
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-05-18 08:03:45 -0600

    1e8 to 1e5

--- __tests__/unit/crypto/constants.test.ts
+++ __tests__/unit/crypto/constants.test.ts
@@ -4,6 +4,6 @@ import * as constants from "../../../packages/crypto/src/constants";
 describe("Constants", () => {
     it("satoshi is valid", () => {
         expect(constants.SATOSHI).toBeDefined();
-        expect(constants.SATOSHI).toBe(100000000);
+        expect(constants.SATOSHI).toBe(100000);
     });
 });
