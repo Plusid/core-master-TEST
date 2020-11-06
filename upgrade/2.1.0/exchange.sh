a5d639d5326a87ead6e88a4027e8c7479113fa74
Infinity Software Ltd <31319908+Plusid@users.noreply.github.com>
2020-11-06 16:05:19 +0400

    Update exchange.sh

--- upgrade/2.1.0/exchange.sh
+++ upgrade/2.1.0/exchange.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-cd ~/ark-core
-pm2 delete ark-core
-pm2 delete ark-core-relay
+cd ~/core-master
+pm2 delete inf-core
+pm2 delete inf-core-relay
 git reset --hard
 git pull
 git checkout master
 yarn run bootstrap
 yarn run upgrade
 
-pm2 --name 'ark-core-relay' start ~/ark-core/packages/core/dist/index.js -- relay --network mainnet
+pm2 --name 'inf-core-relay' start ~/inf-core/packages/core/dist/index.js -- relay --network mainnet
