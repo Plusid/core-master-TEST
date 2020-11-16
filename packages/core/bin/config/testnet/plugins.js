1cda35fd86a95c07d957563bd45dc993caed93b8
Deployer from Infinity Blockchains <support@infinitysoftware.io>
2020-11-16 13:13:30 +0000

    prepare new network config 🎉 Infinity

--- packages/core/bin/config/testnet/plugins.js
+++ packages/core/bin/config/testnet/plugins.js
@@ -3,12 +3,12 @@ module.exports = {
     "@arkecosystem/core-logger-pino": {},
     "@arkecosystem/core-p2p": {
         server: {
-            port: process.env.CORE_P2P_PORT || 4000,
+            port: process.env.CORE_P2P_PORT || 4002,
         },
-        minimumNetworkReach: 5,
+        minimumNetworkReach: 2,
     },
     "@arkecosystem/core-state": {},
-    "@arkecosystem/core-magistrate-transactions": {},
+    
     "@arkecosystem/core-database-postgres": {
         connection: {
             host: process.env.CORE_DB_HOST || "localhost",
@@ -23,27 +23,18 @@ module.exports = {
         maxTransactionsPerSender: process.env.CORE_TRANSACTION_POOL_MAX_PER_SENDER || 300,
         allowedSenders: [],
         dynamicFees: {
-            enabled: true,
-            minFeePool: 1000,
-            minFeeBroadcast: 1000,
+            enabled: false,
+            minFeePool: 1,
+            minFeeBroadcast: 1,
             addonBytes: {
-                transfer: 100,
+                transfer: 1,
                 secondSignature: 250,
                 delegateRegistration: 400000,
                 vote: 100,
                 multiSignature: 500,
                 ipfs: 250,
                 multiPayment: 500,
-                delegateResignation: 100,
-                htlcLock: 100,
-                htlcClaim: 0,
-                htlcRefund: 0,
-                businessRegistration: 4000000,
-                businessUpdate: 500,
-                businessResignation: 100,
-                bridgechainRegistration: 4000000,
-                bridgechainUpdate: 500,
-                bridgechainResignation: 100,
+                delegateResignation: 400000,
             },
         },
     },
